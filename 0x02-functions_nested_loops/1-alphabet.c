d005bf61c7bd72cfc7cab8f891ebfc1ecf24541d
root <raul.fm.mateus@gmail.com>
2022-12-14 15:27:15 -0800

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,17 +6,17 @@
  * Return: 0
  */
 
-void print_alphabet(void);
+void print_alphabet (void);
 
 {
 	char c = 'a';
 
 	while (c <= 'z')
 	{
-		_putchar(c);
+		_putchar (c);
 		c++;
 	}
 
-	_putchar('\n');
+	_putchar ('\n');
 
 }
